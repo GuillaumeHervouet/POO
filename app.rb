94d352ab3c27807fb4ea12212e260cf261bccfe2
GuillaumeHervouet <146120945+GuillaumeHervouet@users.noreply.github.com>
2023-10-18 09:41:41 +0200

    Add files via upload

---
+++ app.rb
@@ -0,0 +1,26 @@
+require 'pry'
+require 'bundler'
+Bundler.require
+
+require_relative 'lib/game'
+require_relative 'lib/player'
+
+puts "---------------------------------------------------"
+puts "| Bienvenue sur 'ILS VEULENT TOUS MA POO' !       |"
+puts "| Le but du jeu est d'être le dernier survivant ! |"
+puts "---------------------------------------------------"
+
+# Saisie du nom du premier joueur
+puts "Entrez le nom du premier joueur:"
+player1_name = gets.chomp
+
+# Saisie du nom du deuxième joueur
+puts "Entrez le nom du deuxième joueur:"
+player2_name = gets.chomp
+
+game = Game.new(player1_name, player2_name)
+
+# (Optionnel)
+#binding.pry 
+
+game.combat
